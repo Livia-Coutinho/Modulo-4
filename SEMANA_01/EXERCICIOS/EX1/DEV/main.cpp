0881758c9f2d66356a14c5a109f1fa721710d73d
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2022-10-18 15:32:33 -0300

    ex. 1

--- SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
+++ SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
@@ -1,63 +1,62 @@
+//Questão 1 --> Realizar um ajuste (a partir de um min, um max e uma medida)	//////////////////
+//Questão 2 --> Ler um valor											        //////////////////
+//Questão 3 --> Função que guarde um valor a um vetor					        /////////////////
+//Questão 4 --> Pergunta e armazena as direções						            /////////////////
+//Questão 5 --> Deseja continuar										        /////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 #include <string>
 
-// 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente 
-// entre dois valores mínimo e máximo e retorna esse valor.
+// 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente entre dois valores 
+//mínimo e máximo e retorna esse valor.
 
-
-
+int ajusteDosValores (int minimo, int maximo, int medida) {
+	medida = medida - minimo;
+	int variacao = maximo - minimo;
+	int resultado = medida/variacao;
+	return resultado;
+}
 			
-// 2 - Faça uma função que simule a leitura de um sensor lendo o 
-// valor do teclado ao final a função retorna este valor
+// 2 - Faça uma função que simule a leitura de um sensor lendo o  valor do teclado.
+//Ao final a função retorna este valor
 
-// 3 - Faça uma função que armazena uma medida inteira qualquer 
-// em um vetor fornecido. Note que como C não possui vetores 
-// nativos da linguagem, lembre-se que você precisa passar o 
-// valor máximo do vetor assim como a última posição preenchida
-// Evite também que, por acidente, um valor seja escrito em 
-// uma área de memória fora do vetor
+// 3 - Faça uma função que armazena uma medida inteira qualquer  em um vetor fornecido. 
+//Note que como C não possui vetores nativos da linguagem, lembre-se que você precisa passar o valor máximo do 
+//vetor assim como a última posição preenchida.
+// Evite também que, por acidente, um valor seja escrito em uma área de memória fora do vetor
 
-
-
-// 4 - Faça uma função que recebe um vetor com 4 posições que contém 
-// o valor da distância de um pequeno robô até cada um dos seus 4 lados.
-// A função deve retornar duas informações: A primeira é a direção 
-// de maior distância ("Direita", "Esquerda", "Frente", "Tras") e a 
-// segunda é esta maior distância.
-
-
-
+// 4 - Faça uma função que recebe um vetor com 4 posições que contém o valor da distância de um pequeno robô 
+//até cada um dos seus 4 lados.
+// A função deve retornar duas informações: A primeira é a direção  de maior distância 
+//("Direita", "Esquerda", "Frente", "Tras") e a segunda é esta maior distância.
 
 // 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e 
 // retorna verdadeiro ou falso
 
+// 6 - A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô e através de 4 sensores em [
+//cada um dos 4 pontos do robo ("Direita", "Esquerda", "Frente", "Tras"). 
+// A cada passo, ele verifica as distâncias aos objetos e vai mapeando o terreno para uma movimentação futura. 
+// Ele vai armazenando estas distancias em um vetor fornecido como parâmetro e retorna a ultima posicao 
+//preenchida do vetor.
+//Esta função deve ir lendo os 4 sensores até que um comando de pare seja enviado.
 
+// Para simular os sensores e os comandos de pare, use as funções já construídas nos ítens anteriores e 
+//em um looping contínuo até que um pedido de parada seja enviado pelo usuário. 
 
+int soma (int a, int b){
+	return a+b;
+}
 
-
-
-// 6 - A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô 
-// e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", 
-// "Frente", "Tras"). 
-//      A cada passo, ele verifica as distâncias aos objetos e vai mapeando o terreno 
-// para uma movimentação futura. 
-//      Ele vai armazenando estas distancias em um vetor fornecido como parâmetro 
-// e retorna a ultima posicao preenchida do vetor.
-//      Esta função deve ir lendo os 4 sensores até que um comando de pare seja enviado 
-//
-//      Para simular os sensores e os comandos de pare, use as funções já construídas 
-// nos ítens anteriores e em um looping contínuo até que um pedido de parada seja 
-// enviado pelo usuário. 
-//
-//      Complete a função com a chamada das funções já criadas
-int dirige(int *v,int maxv){
-	int maxVetor = maxv;
-	int *vetorMov = v;
+//Complete a função com a chamada das funções já criadas
+int dirige(int *v,int maxv){ // 
+	int maxVetor = maxv; //maxvetor recebe maxv  //maxVetor  --> tamanhoMaximoVetor
+	int *vetorMov = v; //asterisco - ponteiro - referência no espaço da memória
 	int posAtualVetor = 0;
 	int dirigindo = 1;		
 	while(dirigindo){		
 		int medida = /// .. Chame a função de de leitura da medida para a "Direita"
-		medida = converteSensor(medida,0,830);
+		medida = converteSensor(medida,0,830); //(Troquei )
 		posAtualVetor = // Chame a função para armazenar a medida no vetor
         ///////////////////////////////////////////////////////////////////////////		
 		// Repita as chamadas acima para a "Esquerda", "Frente", "Tras"
@@ -69,11 +68,10 @@ int dirige(int *v,int maxv){
 }
 
 
-// O trecho abaixo irá utilizar as funções acima para ler os sensores e o movimento
-// do robô e no final percorrer o vetor e mostrar o movimento a cada direção baseado 
-// na maior distância a cada movimento
+// O trecho abaixo irá utilizar as funções acima para ler os sensores e o movimento do robô e no final 
+//percorrer o vetor e mostrar o movimento a cada direção baseado na maior distância a cada movimento.
 void percorre(int *v,int tamPercorrido){		
-	int *vetorMov = v;
+	int *vetorDeMovimentos = v; //Troquei vetorMov por Vetor de Movimentos
 	int maiorDir = 0;
 	
 	for(int i = 0; i< tamPercorrido; i+=4){
