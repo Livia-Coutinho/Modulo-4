6a0a9347da9e36feb9763eaadddaf2f5c41f8f46
Livia Coutinho <livia.coutinho@sou.inteli.edu.br>
2022-11-27 22:41:15 -0300

    Update - Tic Tac Toe

--- SEMANA_07/EXERCICIOS/Tic-Tac-Toe Game S7.jsx
+++ SEMANA_07/EXERCICIOS/Tic-Tac-Toe Game S7.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react'
+const heading = <h1> Mozilla Developer Network</h1>;
+
+import React, {useState} from 'react';
 
 export default function App(){
     //Estilização do game
 
-    const tabuleiro={ //tabu
+    const tabu={ //tabu
         display: 'flex',
         flexDirection: 'column'  //tabuleiro em coluna
     }
@@ -15,14 +17,15 @@ export default function App(){
 
     const casinha={ //casa //9casinhas //3 linhas e 3 colunas //casa que recebe os símbolos
         width: 100,
-        height: 100,
+        height: 100, 
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
         cursor: 'pointer',
         fontSize: 60,
-        border: '1px solid #000'
+        border: '1px solid #000',
+        minHeight: 100 //opcional
     }
 
     const jogoinicial=[['', '', ''], ['', '', ''], ['', '', '']]//trocar o nome //jogo da velha é um array
@@ -39,13 +42,13 @@ export default function App(){
                 //o jagando será 'false' quando algupem ganhar ou quando terminar em "velha".
                 //Nesses casos o jogando recebe false
 
-    const Tabuleiro= ()=>{ //nometrocar
+    const tabuleiro= ()=>{ //nometrocar
         //função que retorna o tabuleiro
         //arrow function que retorna o tabuleiro do game
         //o tabuleiro retornada já está preenchido com a jogada
         //logo, para isso, o tabuleiro terá que receber o jogo
         return(
-            <div style= {tabuleiro}>
+            <div style= {tabu}>
             {/* /*pegando a estilização registrada no const tabuleiro no ínicio do código */}
             {/* //retorna a div com o style tabuleiro */}
                 <div style={LinhasDoTabuleiro}> {/*linha um - index 0*/}
@@ -78,6 +81,16 @@ export default function App(){
             </div>
         )
     }
+
+    //trocar nome //botão    
+    const BtnJogarNovamente=()=>{
+        if(!jogando){ //! - not
+            return<button onClick={()=>reiniciar}>Jogar mais uma vez</button>
+        }
+    }
+
+
+
 
     //função que realiza checagem sobre o jogador ter ganhado
     const verificaVitoria=()=>{   //trocarnome
@@ -119,12 +132,11 @@ export default function App(){
         //diagonais
         pontos=0
         for (let d=0; d <3; d++){
-            if(jogo[d][d]){
-                if(jogo[l][c]==simboloAtual){
-                    pontos++
-                }
+            if(jogo[d][d]== simboloAtual){
+                pontos++
             }
         }
+    }
         if(pontos >= 3){ 
             vitoria=true //se a pontuação for igual a 3 = vitória
             //return    
@@ -145,6 +157,71 @@ export default function App(){
         return vitoria
     }
 
-}
+    //função - troca de jogador
+    const trocaJogador=()=>{
+        simboloAtual=='X'?setSimboloAtual('O'):setSimboloAtual('X')
+        
+    }
+
+    //retornar a posição
+    const retPos=()=>{
+        const p=e.target.getAttribute('data-pos')//declarado no começo do código
+        const pos=[parseInt(p.substring(0,1)), parseInt(p.substring(1,2))] //inteiro
+        return pos
+    }
+
+    //trocar nome //para verificar se posso ou não jogar na posição
+    const VerificaEspacoVazio=(e)=>{
+        if(jogo[retPos(e)[0]][retPos(e)[1]]==''){ //se vazio
+            return true
+        } else{
+            return false //posição não está vazia
+        }
+    }
+
+    const joga=(e)=>{
+        if(jogando){
+            if(VerificaEspacoVazio(e)){
+                jogo[retPos(e)[0], retPos(e)[1]]=simboloAtual
+                trocaJogador()
+                if(verificaVitoria()){
+                    trocaJogador()
+                    alert('Jogador' + simboloAtual + 'venceu')
+                    setJogando(false)
+                }
+
+            }else{
+                alert('A posição já encontra-se marcada. Jogue novamente num espaço vazio')
+            }
+        }
+    }
+
+    //trocar o nome
+    const reiniciar=()=>{
+        setJogando(true)
+        setJogo(jogoinicial)
+        setSimboloAtual('X')
+    }
+
+
+    return(
+        <>
+            <div>
+                <p>Quem joga: {simboloAtual} </p>
+            </div>
+            <div>
+                {tabuleiro(jogo)}
+            </div>
+            <div>
+                {BtnJogarNovamente()}
+            </div>
+        
+        </>
+    )
+
+
+
+
+
 
     
